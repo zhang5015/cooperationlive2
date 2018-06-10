eb3d79f288634f7e780977435711b275b52645e9
Zhang Jun <meilibudazhe741@163.com>
2018-06-10 23:27:13 +0800

    20180610

--- plugins/sfDoctrineGuardPlugin/modules/sfGuardUser/actions/actions.class.php
+++ plugins/sfDoctrineGuardPlugin/modules/sfGuardUser/actions/actions.class.php
@@ -13,4 +13,17 @@ require_once dirname(__FILE__).'/../lib/sfGuardUserGeneratorHelper.class.php';
  */
 class sfGuardUserActions extends autoSfGuardUserActions
 {
+	public function executeMobile($request)
+	{
+		$username = $request->getParameter("username");
+		$password = $request->getParameter("password");
+
+		$response->setContentType("text/json;charset=UTF-8");
+		$returnValue = Array(
+            "user",
+		$username
+		);
+
+		return json_encode($returnValue);
+	}
 }
